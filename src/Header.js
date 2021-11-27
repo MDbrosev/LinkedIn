4256c9c6b53c366cf6875054a66464d334905552
Yoseph Hasson <yoseph_hasson@yahoo.com>
2021-11-27 16:48:19 -0500

    building out header bar, logo, sticky, and more

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import CompanyLogo from './images/linkedin.png';
+import HeaderOptions from './HeaderOptions';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import profilePic from './images/v0caqchbtn741.jpg';
 
 function Header() {
     return (
-        <div>
-            <h1>This is the header</h1>
+        <div className="header">
+            {/* Company Logo */}
+            <div className="header_left">
+                <img src={CompanyLogo} alt=""/>
+
+                {/* Search Bar*/}
+                <div className="header_search">
+                    <SearchIcon/>
+                    <input type="text"/>
+                </div>
+            </div>
+
+            {/*Header Options*/}
+            <div className="header_right">
+                <HeaderOptions Icon={HomeIcon} title="Home"/>
+                <HeaderOptions Icon={SupervisorAccountIcon} title="My Network"/>
+                <HeaderOptions Icon={BusinessCenterIcon} title="Jobs"/>
+                <HeaderOptions Icon={ChatIcon} title="Messaging"/>
+                <HeaderOptions Icon={NotificationsIcon} title="Notifications"/>
+                <HeaderOptions avatar={profilePic} title="me"/>
+            </div>
         </div>
     )
 }
