961f31e53bf7ada769191e1536c0333d7932255f
Yoseph Hasson <yoseph_hasson@yahoo.com>
2021-12-04 10:21:08 -0500

    Adding animation to feed

--- src/features/Feed/Feed.js
+++ src/features/Feed/Feed.js
@@ -10,7 +10,8 @@ import Post from "../Post/Post";
 import { db } from "../../firebase";
 import firebase from 'firebase/compat/app';
 import { useSelector } from 'react-redux';
-import { selectUser } from "../User/userSlice"
+import { selectUser } from "../User/userSlice";
+import FlipMove from 'react-flip-move';
 
 function Feed() {
     const user = useSelector(selectUser);
@@ -59,16 +60,17 @@ function Feed() {
                 <InputOption Icon = {CalendarViewDayIcon} title="Write article" color="#7FC15E"/>
                 </div>
             </div>
-
-            {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
-                <Post 
-                    key = {id}
-                    name = {name}
-                    description = {description}
-                    message = {message}
-                    photoUrl = {photoUrl}
-                />
-            ))}
+            <FlipMove>
+                {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+                    <Post 
+                        key = {id}
+                        name = {name}
+                        description = {description}
+                        message = {message}
+                        photoUrl = {photoUrl}
+                    />
+                ))}
+            </FlipMove>
         </div>
     );
 }
--- src/features/Post/Post.js
+++ src/features/Post/Post.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import './Post.css';
 import { Avatar } from "@material-ui/core";
 import InputOption from '../InputOption/InputOption';
 import { ChatOutlined, SendOutlined, ShareOutlined, ThumbUpAltOutlined } from '@material-ui/icons';
 
-function Post({ name, description, message, photoUrl }) {
+const Post = forwardRef(({ name, description, message, photoUrl }, ref) => {
 
     return (
-        <div className="post">
+        <div ref={ref} className="post">
             <div className="post_header">
                 <Avatar src={photoUrl}>{name[0]}</Avatar>
                 <div className="post_info">
@@ -27,6 +27,6 @@ function Post({ name, description, message, photoUrl }) {
             </div>
         </div>
     );
-}
+})
 
 export default Post
