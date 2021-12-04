c1f38fde706febf5f2626829fc49a33d43ec6611
Yoseph Hasson <yoseph_hasson@yahoo.com>
2021-12-04 09:42:22 -0500

    Conditional rendering on header

--- src/features/HeaderOptions/HeaderOptions.js
+++ src/features/HeaderOptions/HeaderOptions.js
@@ -7,11 +7,22 @@ import { selectUser } from '../User/userSlice'
 function HeaderOptions({avatar, Icon, title, onClick }) {
     const user = useSelector(selectUser);
     
+    if(user){
+        return (
+            <div onClick={onClick} className="headerOption">
+                {Icon && <Icon className="headerOption_icon"/>}
+                {avatar && (
+                    <Avatar className='headerOption_icon' src={user?.photoUrl} >{user.email[0]}</Avatar>
+                )}
+                <h3 className="headerOption_title">{title}</h3>
+            </div>
+        )
+    }
     return (
         <div onClick={onClick} className="headerOption">
             {Icon && <Icon className="headerOption_icon"/>}
             {avatar && (
-                <Avatar className='headerOption_icon' src={avatar} />
+                <Avatar className='headerOption_icon' src={user?.photoUrl} ></Avatar>
             )}
             <h3 className="headerOption_title">{title}</h3>
         </div>
